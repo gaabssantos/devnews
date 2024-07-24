d5e28a3738b2c11964b2af68304963a17d0c370c
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-24 11:53:29 -0300

    refactor(Zod Schema created): :recycle:

--- devnews-server/src/dtos/user.dto.ts
+++ devnews-server/src/dtos/user.dto.ts
@@ -1,6 +1,15 @@
+import { z } from 'zod';
+
 export type UserDTO = {
   name: string;
   email: string;
   role: 'user' | 'editor';
   password: string;
+};
+
+export const userSchema = {
+  name: z.string().regex(/[A-z]{3}[' '][A-z]{3}/),
+  email: z.string().email(),
+  role: z.enum(['user', 'editor']),
+  password: z.string().trim().min(8),
 };
--- devnews-server/src/routes/user.route.ts
+++ devnews-server/src/routes/user.route.ts
@@ -2,7 +2,9 @@ import { Router } from 'express';
 
 import { UserController } from '../controllers/user.controller';
 import { UserSessionController } from '../controllers/user.session.controller';
+import { userSchema } from '../dtos/user.dto';
 import { UserFactory } from '../factories/user.factory';
+import { ParamsType, validator } from '../middlewares/validator.middleware';
 
 export const userRoute = Router();
 
@@ -11,5 +13,9 @@ const userSessionController = new UserSessionController(
   UserFactory.getServiceInstance(),
 );
 
-userRoute.post('/', userController.create);
+userRoute.post(
+  '/',
+  validator({ schema: userSchema, type: ParamsType.BODY }),
+  userController.create,
+);
 userRoute.post('/session', userSessionController.create);
