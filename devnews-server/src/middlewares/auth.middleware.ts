2a2723931e9dbef595d913f7b5243988a661ff7d
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-25 10:07:54 -0300

    feat(Checking auth token): :sparkles:

---
+++ devnews-server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+import { AppError } from '../errors/app.error';
+
+interface MyUserRequest extends Request {
+  userId?: string;
+  userEmail?: string;
+}
+
+interface IJWTPayload extends JwtPayload {
+  id: string;
+  email: string;
+}
+
+export default (req: MyUserRequest, res: Response, next: NextFunction) => {
+  const authToken = req.headers.authorization;
+  const authSecret = process.env.JWT_SECRET;
+
+  if (!authToken)
+    throw new AppError(StatusCodes.UNAUTHORIZED, 'token_not_provied');
+
+  const token = authToken.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, authSecret as string) as IJWTPayload;
+
+    req.userId = decoded.id;
+    req.userEmail = decoded.email;
+
+    return next();
+  } catch (err) {
+    throw new AppError(StatusCodes.UNAUTHORIZED, 'token_is_invalid');
+  }
+};
