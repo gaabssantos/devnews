42d5ff737d4d5a5fbcf49fa0794a3c25aebabec0
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-30 10:50:22 -0300

    refactor(Fix created post bug): :bug:

--- devnews-server/src/entities/post.entity.ts
+++ devnews-server/src/entities/post.entity.ts
@@ -1,9 +1,11 @@
+import { CategoryEntity } from './category.entity';
+
 type PostProps = {
   _id?: string;
   title: string;
   content: string;
   image: string;
-  category: string;
+  category: CategoryEntity;
 };
 
 export class PostEntity {
@@ -11,7 +13,7 @@ export class PostEntity {
   public title: string;
   public content: string;
   public image: string;
-  public category: string;
+  public category: CategoryEntity;
 
   constructor({ _id, title, content, image, category }: PostProps) {
     this._id = _id;
--- devnews-server/src/services/post.service.ts
+++ devnews-server/src/services/post.service.ts
@@ -1,16 +1,28 @@
+import { StatusCodes } from 'http-status-codes';
+
+import { CategoryRepository } from '../database/repositories/category.repository';
 import { PostRepository } from '../database/repositories/post.repository';
+import { CategoryModel } from '../database/schemas/category.schema';
 import { PostDTO } from '../dtos/post.dto';
 import { PostEntity } from '../entities/post.entity';
+import { AppError } from '../errors/app.error';
 
 export class PostService {
   constructor(private postRepository: PostRepository) {}
 
   create = async ({ title, content, image, category }: PostDTO) => {
+    const categoryRepository = new CategoryRepository(CategoryModel);
+    const foundCategory = await categoryRepository.findByName(category);
+
+    if (!foundCategory) {
+      throw new AppError(StatusCodes.BAD_REQUEST, 'category_not_exists');
+    }
+
     const post = new PostEntity({
       title,
       content,
       image,
-      category,
+      category: foundCategory,
     });
 
     const postCreated = await this.postRepository.create(post);
