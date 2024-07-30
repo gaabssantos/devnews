96205fd59b129c2c6fba5215777bc3089636e5e0
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-30 09:19:05 -0300

    style(Created themes and globalStyles): :lipstick:

---
+++ devnews-client/src/styles/themes.styles.ts
@@ -0,0 +1,22 @@
+export default {
+  colors: {
+    primary: '#CEDEED',
+    secondary: '#0066C3',
+    terciary: '#00294F',
+    gradiant:
+      'linear-gradient(180deg, rgba(0, 84, 161, 0.64) 0%, rgba(0, 84, 161, 0.00) 100%)',
+    white: '#FFFFFF',
+    black: '#000000',
+  },
+  fonts: {
+    family: {
+      main: 'Montserrat, sans-serif',
+    },
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      big: '4rem',
+    },
+  },
+};
